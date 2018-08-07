7fc48e927fcbbac375777890795d217f33bf843f
Ransford Walker <ransfordw@gmail.com>
2018-08-07 11:45:15 -0400

    intitial commit

--- FoodNStuff.WebMVC/Models/IdentityModels.cs
+++ FoodNStuff.WebMVC/Models/IdentityModels.cs
@@ -29,5 +29,11 @@ namespace FoodNStuff.WebMVC.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+
     }
 }
